881c857a09a222de92bbca7b10d2bca29509ae93
Szczepan <stephensocha@yahoo.com>
2020-11-24 21:14:41 +0100

    add findAll method

--- src/main/java/com/ordersmanager/om/controller/ViewController.java
+++ src/main/java/com/ordersmanager/om/controller/ViewController.java
@@ -3,17 +3,28 @@ package com.ordersmanager.om.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.ordersmanager.om.client.service.ClientService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ViewController {
 
-     private ClientService clientService;
+     private final ClientService clientService;
 
     @Autowired
     public ViewController(ClientService clientService) {
         this.clientService = clientService;
+    }
+
+
+
+    @GetMapping("/clients")
+    public String getAllClients(Model model) {
+        model.addAttribute("clients", clientService.findAll());
+
+        return "client-list";
     }
 
     @PostMapping("/add")
